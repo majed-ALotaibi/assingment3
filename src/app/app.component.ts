b4f9d969d07e4370d8a243cd18ba9684a12426f8
majed <al8iisr@gmail.com>
2021-04-24 08:28:44 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assingment';
+
+constructor(){
+
+}
+showingState= true;
+btncount = [0];
+n:number =0 ;
+Action(){
+  this.n++;
+ this.showingState = !this.showingState;
+this.btncount.push(this.n);
+}
+getColor(){
+  if (this.n>=5) {
+    return"blue";
+  } else {
+    return "none";
+  }
+}
 }
